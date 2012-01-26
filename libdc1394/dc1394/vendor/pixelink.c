0bcdc9e62bf434842cd4dfee72d4045892c08a03
Damien Douxchamps <ddouxchamps@users.sf.net>
2012-01-26 13:33:47 +0900

    new windows driver

--- libdc1394/dc1394/vendor/pixelink.c
+++ libdc1394/dc1394/vendor/pixelink.c
@@ -24,10 +24,15 @@
  * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "config.h"
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#ifdef HAVE_WINDOWS
+#include <windows.h>
+#else
 #include <arpa/inet.h>
+#endif
 
 #include "vendor/pixelink.h"
 #include "log.h"
